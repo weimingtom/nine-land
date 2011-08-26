4eebfe19c347d617b32472e437b5ccab725e2a21
zhao.jie.0 <Zhao.Jie.0@gmail.com>
2011-08-26 09:50:51 +0000

    blahblah

--- trunk/nine-land/trunk/test/gen/my/first/R.java
+++ trunk/nine-land/trunk/test/gen/my/first/R.java
@@ -18,14 +18,17 @@ public final class R {
     }
     public static final class id {
         public static final int btn1=0x7f060003;
+        public static final int button1=0x7f060007;
         public static final int editText1=0x7f060005;
         public static final int number1=0x7f060000;
         public static final int number2=0x7f060002;
         public static final int rst=0x7f060004;
         public static final int run=0x7f060001;
+        public static final int second_text=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int second=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040002;
--- trunk/nine-land/trunk/test/src/my/first/TestActivity.java
+++ trunk/nine-land/trunk/test/src/my/first/TestActivity.java
@@ -1,9 +1,16 @@
 package my.first;
 
+import my.first.view.MyView;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,13 +19,54 @@ import android.widget.Toast;
 
 public class TestActivity extends Activity {
     /** Called when the activity is first created. */
+    final static protected int MENU_ABOUT = Menu.FIRST;
+    final static protected int MENU_QUIT = Menu.FIRST + 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        MyView myView = new MyView(this);
+        setContentView(myView);
 
-        Button btn = (Button) findViewById(R.id.btn1);
-        btn.setOnClickListener(this.onClick);
+//        Button btn = (Button) findViewById(R.id.btn1);
+//        btn.setOnClickListener(this.onClick);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, MENU_ABOUT, 0, "关于");
+        menu.add(0, MENU_QUIT, 1, "退出");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+        case MENU_ABOUT:
+//            this.openUri();
+            Intent intent = new Intent();
+            intent.setClass(TestActivity.this, Second.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("key", "from bundle");
+            intent.putExtras(bundle);
+            
+            startActivity(intent);
+            break;
+        case MENU_QUIT:
+            this.finish();
+            break;
+        }
+        return true;
+    }
+
+    public void openUri() {
+        Uri uri = Uri.parse("http://urs.mmou.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 
     private OnClickListener onClick = new OnClickListener() {
@@ -48,6 +96,9 @@ public class TestActivity extends Activity {
             Toast pop = Toast.makeText(TestActivity.this, "oh hohoho",
                     Toast.LENGTH_SHORT);
             pop.show();
+            Uri uri = Uri.parse("http://urs.mmou.com");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
 
         }
     };
